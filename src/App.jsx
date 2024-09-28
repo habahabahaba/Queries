516b93032fa81869d243507f0ad3cd80ccdeda51
Alex S <habahabahaba@outlook.com>
2024-09-28 06:01:54 +0400

    Add QueryClientProvider to the App.jsx. Extract fetchEvents function. Refactor NewEventsSection.jsx to use React-Query.

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
+// React-Query:
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+// React-Router:
 import {
   Navigate,
   RouterProvider,
   createBrowserRouter,
 } from 'react-router-dom';
 
+// Components:
 import Events from './components/Events/Events.jsx';
 import EventDetails from './components/Events/EventDetails.jsx';
 import NewEvent from './components/Events/NewEvent.jsx';
 import EditEvent from './components/Events/EditEvent.jsx';
 
+const queryClient = new QueryClient();
+
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Navigate to="/events" />,
+    element: <Navigate to='/events' />,
   },
   {
     path: '/events',
@@ -38,7 +44,11 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
