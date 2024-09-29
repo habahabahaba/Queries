7e823c44e43dcacd33a369a1ffef7a5a3f451081
Alex S <habahabahaba@outlook.com>
2024-09-30 01:33:12 +0400

    Add modal window for the delete event inside the EventDetailsHeader.jsx.

--- src/components/Events/EventDetailsHeader.jsx
+++ src/components/Events/EventDetailsHeader.jsx
@@ -5,12 +5,18 @@ import { queryClient } from '../../api/index.js';
 import { deleteEvent } from '../../api/mutations/index.js';
 // React-Router:
 import { Link, useParams, useNavigate } from 'react-router-dom';
+// React:
+import { useState } from 'react';
 
 // Components:
 import ErrorBlock from '../UI/ErrorBlock.jsx';
 import LoadingIndicator from '../UI/LoadingIndicator.jsx';
+import Modal from '../UI/Modal.jsx';
 
 export default function EventDetailsHeader({ title }) {
+  // State:
+  const [isDeleting, setIsDeleting] = useState(false);
+
   // Routing:
   const navigate = useNavigate();
   // Params (for id):
@@ -30,29 +36,56 @@ export default function EventDetailsHeader({ title }) {
       navigate('/events');
     },
   });
+
   // Handlers:
+  function startDeleting() {
+    setIsDeleting(true);
+  }
+  function stopDeleting() {
+    setIsDeleting(false);
+  }
   function handleDelete() {
     mutate({ id });
   }
 
   // JSX:
-  return (
-    <header>
-      <h1>{title}</h1>
+  const deleteModal = (
+    <Modal onClose={stopDeleting}>
       {isPending ? (
-        <LoadingIndicator />
-      ) : (
-        <nav>
-          <button onClick={handleDelete}>Delete</button>
-          <Link to='edit'>Edit</Link>
-        </nav>
-      )}
-      {isError ? (
+        <>
+          <span>Deleting the event...</span> <LoadingIndicator />
+        </>
+      ) : isError ? (
         <ErrorBlock
           title='An error occurred'
           message={error.info?.message || 'Failed to delete this event.'}
         />
-      ) : null}
+      ) : (
+        <>
+          <h2>Do you really want to delete this event?</h2>
+          <p>This action can not be undone!</p>
+          <div className='form-actions'>
+            <button onClick={stopDeleting} className='button-text'>
+              Cancel
+            </button>
+            <button onClick={handleDelete} className='button'>
+              Delete
+            </button>
+          </div>
+        </>
+      )}
+    </Modal>
+  );
+
+  return (
+    <header>
+      <h1>{title}</h1>
+
+      <nav>
+        <button onClick={startDeleting}>Delete</button>
+        <Link to='edit'>Edit</Link>
+      </nav>
+      {isDeleting ? deleteModal : null}
     </header>
   );
 }
